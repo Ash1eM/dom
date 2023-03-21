d1094210d982ec5bacdccb7250a085705dbeddc0
Ash1eM <ashley.morris101@yahoo.com>
2023-03-20 20:56:45 -0400

    jokes

---
+++ dom/dom.js
@@ -0,0 +1,10 @@
+const main = document.querySelector('main')
+const joke = "What did the horse say after it tripped? Help! I've fallen and I can't giddyup!";
+const joke2 = "Why can't you hear a pterodactyl going to the bathroom? because the P is silent";
+const joke3 = "Why did the bicycle fall over? because it was two tired";
+const myTemp= `
+  <h1>My Jokes</h1>
+  <li> ${joke} </li>
+  <li> ${joke2} </li>
+  <li> ${joke3} </li>
+`;
